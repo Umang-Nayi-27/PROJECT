9e8fd2cb9ecee253bac9a783d0f00948aebf20d4
Umang-Nayi-27 <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-08-18 10:04:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,35 @@
-gsap.to("#hl1",{x:-6300 , duration:15 , repeat:-1 , ease:"linear"})
-
-
-gsap.to("#page1",{scrollTrigger:{trigger:"#page1",start:"top top", end:"bottom 50%" , pin:true, }})
-
-// ----------------------------------------------------------------- PIN PAGES ---------------------------------------------
-
-gsap.to(
-    "#page2",{scrollTrigger:{
-      trigger:"#page2",
-      start:`top top`,
-      end:`bottom 50%`,
-      pin:true,
-      }}
-  )
-
-  gsap.to(
-    "#page3",{scrollTrigger:{
-      trigger:"#page3",
-      start:`top top`,
-      end:`bottom 50%`,
-      pin:true,
-      
-    }}
-  )
-
-  gsap.to(
-    "#page4",{scrollTrigger:{
-      trigger:"#page4",
-      start:`top top`,
-      end:`bottom 50%`,
-      pin:true,
-    }}
-  )
-
   // -------------------------------------------------------------- PAGE 1-------------------------------------------------------
 
+  gsap.to("#hl1",{x:-6300 , duration:15 , repeat:-1 , ease:"linear"})
+
   gsap.set("#qt1",{opacity:0,y:40})
-  gsap.to("#qt1 ",{opacity:1, y:0,scrollTrigger:{trigger:"#qt1",start:"top 58%", end:"bottom 50%" ,ease:Power3.easeOut,duration:1,scrub:1}})
+  gsap.to("#qt1 ",{opacity:1,y:0,duration:2,scrub:true})
 
   gsap.set("#qt2",{opacity:0,y:40})
-  gsap.to("#qt2",{opacity:1, y:0,scrollTrigger:{trigger:"#qt2",start:"top 58%", end:"bottom 50%" ,ease:Power3.easeOut,duration:1,scrub:1}})
+  gsap.to("#qt2",{opacity:1,y:0,duration:2,scrub:true})
 
-  gsap.to("#page1",{opacity:0,scrollTrigger:{trigger:"#page1",start:"bottom 60%",ease:Power3.easeOut, end:"bottom top" ,duration:3,scrub:1}})   // for page1 opacity decrease
+  // gsap.to("#page1",{opacity:0, scrollTrigger:{trigger:"#page1", start:"center top", end:"bottom top",duration:1}})
 
   // -------------------------------------------------------------- PAGE 2-------------------------------------------------------
 
   gsap.set("#p2pic",{opacity:0,y:100})
-  gsap.to("#p2pic",{opacity:1, y:0,scrollTrigger:{trigger:"#p2pic",start:"top 80%", end:"top 30%" ,duration:4,scrub:1}})  //page 2 picture comes in
+  gsap.to("#p2pic",{opacity:1, y:0,scrollTrigger:{trigger:"#p2pic",start:"top 80%", end:"top 30%" ,duration:4,scrub:1}}) 
 
+  // ------------------------------------------------------------- PAGE 3----------------------------------------------------------
+
+  gsap.set("#p3pic",{opacity:0,y:100})
+  gsap.to("#p3pic",{opacity:1, y:0,scrollTrigger:{trigger:"#p3pic",start:"top 80%", end:"top 30%" ,duration:4,scrub:1}}) 
+
+  // --------------------------------------------------------------- PAGE 4 ------------------------------------------------------------
+
+    gsap.to("#container41",{y:-1000,duration:15,ease:"linear",repeat:-1,yoyo:true})
+    gsap.to("#container42",{y:1000,duration:15,ease:"linear",repeat:-1,yoyo:true})
+
+    // gsap.set("#container31>div>img",{opacity:0})
+    // gsap.to("#container31>div>img",{opacity:1,scrollTrigger:{trigger:"#container31>div>img",duration:2,start:"top bottom",end:"bottom 90%"}})
   
-  gsap.to("#page2",{opacity:0,scrollTrigger:{trigger:"#page2",start:"bottom 60%",ease:Power3.easeOut, end:"bottom top" ,duration:3,scrub:1}})   // for page1 opacity decrease
+    gsap.set("#container41",{opacity:0})
+  gsap.to("#container41",{opacity:1,scrollTrigger:{trigger:"#container31",start:"top 80%", end:"top 30%" ,duration:2,scrub:1}}) 
 
-  // --------------------------------------------------------------- PAGE 3 ------------------------------------------------------------
-
-    gsap.to("#container31",{x:-1000,duration:15,ease:"linear",repeat:-1,yoyo:true})
-    gsap.to("#container32",{x:1000,duration:15,ease:"linear",repeat:-1,yoyo:true})
-  
-    gsap.set("#container31",{opacity:0,y:100})
-    gsap.to("#container31",{opacity:1, y:0,scrollTrigger:{trigger:"#container31",start:"top 90%", end:"top 50%" ,duration:4,scrub:1}})  //page 2 picture comes in
-
-    gsap.set("#container32",{opacity:0,y:100})
-    gsap.to("#container32",{opacity:1, y:0,scrollTrigger:{trigger:"#container32",start:"top 100%", end:"top 85%" ,duration:4,scrub:1}})  //page 2 picture comes in
-
-    gsap.to("#page3",{opacity:0,scrollTrigger:{trigger:"#page3",start:"bottom 60%",ease:Power3.easeOut, end:"bottom top" ,duration:3,scrub:1}})   // for page1 opacity decrease
+  gsap.set("#container42",{opacity:0})
+  gsap.to("#container42",{opacity:1,scrollTrigger:{trigger:"#container32",start:"70% 80%", end:"top 30%" ,duration:2,scrub:1}}) 
