2f4505e2c605ab07a090b77a417099a5d5ee6f29
Umang-Nayi-27 <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-08-18 10:03:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,96 +1,124 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Agdasima&family=Gloria+Hallelujah&family=League+Spartan:wght@100&family=Lobster&family=Orbitron&family=PT+Mono&family=Rajdhani:wght@500&family=Russo+One&family=Share+Tech+Mono&family=Ubuntu:wght@300;400&family=Viga&family=Voltaire&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Agdasima&family=Gloria+Hallelujah&family=League+Spartan:wght@100&family=Lobster&family=Orbitron&family=PT+Mono&family=Rajdhani:wght@500&family=Russo+One&family=Share+Tech+Mono&family=Ubuntu:wght@300;400&family=Viga&family=Voltaire&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Handjet&display=swap" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-16esztaSRplJROstbIIdwX3N97V1+pZvV33ABoG1H2OyTttBxEGkTsoIVsiP1iaTtM8b3+hu2kB6pQ4Clr5yug==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" integrity="sha512-Ic9xkERjyZ1xgJ5svx3y0u3xrvfT/uPkV99LBwe68xjy/mGtO+4eURHZBW2xW4SZbFrF1Tf090XqB+EVgXnVjw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"
+        integrity="sha512-16esztaSRplJROstbIIdwX3N97V1+pZvV33ABoG1H2OyTttBxEGkTsoIVsiP1iaTtM8b3+hu2kB6pQ4Clr5yug=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"
+        integrity="sha512-Ic9xkERjyZ1xgJ5svx3y0u3xrvfT/uPkV99LBwe68xjy/mGtO+4eURHZBW2xW4SZbFrF1Tf090XqB+EVgXnVjw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <body>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
     <div id="nav">
         <img src="img\log2.png" alt="">
-        
     </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
     <div id="nav2">
-    <a href=""><i class="fa-brands fa-x-twitter"></i></a>
-    <a href=""><i class="fa-brands fa-instagram" ></i></a>
-    <a href=""><i class="fa-brands fa-youtube" ></i></a>    
-    <a href=""><i class="fa-brands fa-discord" ></i></a>    
+        <a href=""><i class="fa-brands fa-x-twitter"></i></a>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+        <a href=""><i class="fa-brands fa-youtube"></i></a>
+        <a href=""><i class="fa-brands fa-discord"></i></a>
     </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
     <div id="page1">
         <img src="img\avt1.png" alt="">
+
         <h1 id="hl1">
-            <span >"  Escape to </span>
-            <b >a Universe </b>
+            <span>" Escape to </span>
+            <b>a Universe </b>
             <span>of</span>
-            <i >  Music</i>
-            <span > with Our </span> 
-            <i> Seamless</i> 
+            <i> Music</i>
+            <span> with Our </span>
+            <i> Seamless</i>
             <span>Streaming!"</span>
         </h1>
 
         <div id="qt1">
-            " Explore, Discover, and Dance  to Your Favorite Beats. <br> Welcome to a World of Endless Melodies."
+            " Explore, Discover, and Dance to Your Favorite Beats. <br> Welcome to a World of Endless Melodies."
         </div>
         <div id="qt2">
-            "Welcome to MeloVerse, where music transcends <br> boundaries, melodies paint emotions, and rhythm <br> becomes your language of expression."
+            "Welcome to MeloVerse, where music transcends <br> boundaries, melodies paint emotions, and rhythm <br>
+            becomes your language of expression."
         </div>
     </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
     <div id="page2">
+        <div id="bg">
+            <div id="bg1"></div>
+            <div id="bg2"></div>
+        </div>
         <img src="img/iphone.png" id="p2pic" alt="">
         <div id="p21text">
-            
-        </div>
-    </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
-    <div id="page3">
-        <div id="container31">
-            <div ><img src="img/artist/amit.png" alt=""></div>
-            <div ><img src="img/artist/ar.png" alt=""></div>
-            <div ><img src="img/artist/arijit.jpg" alt=""></div>
-            <div ><img src="img/artist/arman.png" alt=""></div>
-            <div ><img src="img/artist/darshan.png" alt=""></div>
-            <div ><img src="img/artist/harry.png" alt=""></div>
-            <div ><img src="img/artist/jas.png" alt=""></div>
-            <div ><img src="img/artist/sidhu.png" alt=""></div>
-            <div ><img src="img/artist/sonu.png" alt=""></div>
-            <div ><img src="img/artist/udit.png" alt=""></div>
-            <div ><img src="img/artist/yoyo.png" alt=""></div>
 
         </div>
-        <div id="container32">
-            <div ><img src="img/artist/drake.png" alt=""></div>
-            <div ><img src="img/artist/aminem.png" alt=""></div>
-            <div ><img src="img/artist/charlie.png" alt=""></div>
-            <div ><img src="img/artist/dua.png" alt=""></div>
-            <div ><img src="img/artist/ed.png" alt=""></div>
-            <div ><img src="img/artist/justin.png" alt=""></div>
-            <div ><img src="img/artist/martin.png" alt=""></div>
-            <div ><img src="img/artist/selena.png" alt=""></div>
-            <div ><img src="img/artist/weeknd.png" alt=""></div>
-            <div ><img src="img/artist/swift.png" alt=""></div>
-            <div ><img src="img/artist/snake.png" alt=""></div>
+    </div>
+
+    <!-- -------------------------------------------------------------------------------------------------------------------------------------------- -->
+    <div id="page3">
+            
+    </div>
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <div id="page4">
+        <div id="bg">
+            <div id="bg1"></div>
+            <div id="bg2"></div>
+        </div>
+        <div id="cntr">
+            <div id="container41">
+                <div><img src="img/artist/amit.png" alt=""></div>
+                <div><img src="img/artist/ar.png" alt=""></div>
+                <div><img src="img/artist/arijit.jpg" alt=""></div>
+                <div><img src="img/artist/arman.png" alt=""></div>
+                <div><img src="img/artist/darshan.png" alt=""></div>
+                <div><img src="img/artist/harry.png" alt=""></div>
+                <div><img src="img/artist/jas.png" alt=""></div>
+                <div><img src="img/artist/sidhu.png" alt=""></div>
+                <div><img src="img/artist/sonu.png" alt=""></div>
+                <div><img src="img/artist/udit.png" alt=""></div>
+                <div><img src="img/artist/yoyo.png" alt=""></div>
+            </div>
+            <div id="container42">
+                <div><img src="img/artist/drake.png" alt=""></div>
+                <div><img src="img/artist/aminem.png" alt=""></div>
+                <div><img src="img/artist/charlie.png" alt=""></div>
+                <div><img src="img/artist/dua.png" alt=""></div>
+                <div><img src="img/artist/ed.png" alt=""></div>
+                <div><img src="img/artist/justin.png" alt=""></div>
+                <div><img src="img/artist/martin.png" alt=""></div>
+                <div><img src="img/artist/selena.png" alt=""></div>
+                <div><img src="img/artist/weeknd.png" alt=""></div>
+                <div><img src="img/artist/swift.png" alt=""></div>
+                <div><img src="img/artist/snake.png" alt=""></div>
+            </div>
         </div>
     </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
-    <div id="page4">
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <div id="page5">
+
     </div>
-<!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
-     <script src="script.js"></script>
+    <!-- ------------------------------------------------------------------------------------------------------------------------------------ -->
+    <script src="script.js"></script>
 </body>
 
 </html>
