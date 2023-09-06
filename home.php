5155650fee259a6cd9fe27fce29190421048cb51
Umang <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-09-06 23:25:36 +0530

    Update home.php

--- home.php
+++ home.php
@@ -3,11 +3,13 @@
 
 session_start();
 
-echo $_SESSION["sessionuser"];
+
 if (!isset($_SESSION["sessionuser"])) {
     echo "<script> window.location.href='index.php'; </script>";
 } else {
+    echo $_SESSION["path"];
     if (isset($_POST["logout"])) {
+        echo "<script>alert('You are Logged Out.')</script>";
         session_unset();
         session_destroy();
         echo "<script> window.location.href='index.php'; </script>";
@@ -24,24 +26,40 @@ if (!isset($_SESSION["sessionuser"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Include Bootstrap JavaScript (requires jQuery and Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <style>
         /* Reset some default styles */
-        body, h1, h2, h3, p, ul, li {
+        body,
+        h1,
+        h2,
+        h3,
+        p,
+        ul,
+        li {
             margin: 0;
             padding: 0;
+            font-family: 'Viga', sans-serif;
         }
 
         /* Style for the navigation bar */
         .navbar {
-            background-color: #f2c9d3;
+            background-color: white;
             color: black;
             display: flex;
             justify-content: space-between;
@@ -58,13 +76,20 @@ if (!isset($_SESSION["sessionuser"])) {
         .navbar-links {
             list-style: none;
             display: flex;
+            gap: 30px;
         }
 
         .navbar-links li {
             margin-right: 20px;
+            
+        }
+        .navbar-links a:hover {
+            color: #777;            
         }
 
         .navbar-links a {
+            
+            font-weight:normal;
             color: black;
             text-decoration: none;
         }
@@ -80,10 +105,25 @@ if (!isset($_SESSION["sessionuser"])) {
             padding: 10px 20px;
             background-color: #d4cfd0;
             color: black;
-            border: none;
+            border-color: white;
             border-radius: 5px;
             text-decoration: none;
-            cursor: pointer; /* Add cursor style for better interaction */
+            cursor: pointer;
+            transition-delay: 0.2s;
+            
+            /* Add cursor style for better interaction */
+        }
+
+        .navbar-button:hover{
+            
+            background-color: white;
+            border-color: #222;
+        } 
+
+        #prpic{
+            height: 10vh;
+            width: auto;
+            object-fit: cover;
         }
 
         /* Responsive adjustments */
@@ -96,16 +136,18 @@ if (!isset($_SESSION["sessionuser"])) {
                 margin-left: auto;
             }
         }
-
+        /* Other CSS styles ... */
+        /* Responsive adjustments ... */
     </style>
 </head>
+
 <body>
-    <nav class="navbar">
+<nav class="navbar" id="nav">
         <div class="navbar-brand">
-            <a href="#" class="navbar-logo"><b>BeatBoxify</b></a>
+            <img src="img\log2.png" alt="" style=" width: 150px;height: auto;">
         </div>
         <ul class="navbar-links">
-            <li><a href="#">Home</a></li>
+            <li ><a href="#" style="color: #777;"  >Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
@@ -113,41 +155,117 @@ if (!isset($_SESSION["sessionuser"])) {
         <div class="navbar-right">
             <form method="post" action="">
                 <input type="submit" class="navbar-button" name="logout" value="Logout">
-                <input type="submit" class="navbar-button" name="cngpass" value="Change Password">
-                <input type="submit" class="navbar-button" name="updtpr" value="Update Profile">
+                <a type=""  name="updtpr" id="name" style="color:#666; padding:20px ; cursor:pointer" value="">
+                <?php echo $_SESSION["uname"];?></a>
             </form>
         </div>
     </nav>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <!-- ... (carousel content) ... -->
-        <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner" style="height: 80vh;width:100vw;overflow:hidden; background-color:grey">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="/b1.png" alt="First slide">
+        <div id="prof" style="height: 50vh; width:50vw;position:absolute;top:20%;left:20%;z-index:5;background-color:transparent; display:none">
+        <div class="container mt-4" style="background-color: #f2f2f2; padding:20px;">
+        <button type="submit" class="btn btn-primary" id="close" style="background-color: #666; border-color:#222">X</button>
+        <div class="row justify-content-center">
+        
+        
+        
+            <div class="col-md-6">
+            <img src="<?php echo $_SESSION["path"]; ?>" alt="" id="prpic" >
+        
+                <form method="post">
+                    <div class="form-group">
+                        <img src="" alt="">
+                        <label for="username" class="detail">Username ::</label>
+                        <label class="input"><?php 
+                                    echo $_SESSION["uname"];                        
+                                ?></label>
+                        
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="detail">Email  ::</label>
+                        <label class="input"><?php 
+                                    echo $_SESSION["email"];                        
+                                ?></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="fullname" class="detail">Full Name ::</label>
+                        <label  class="input" ><?php 
+                                    echo $_SESSION["fname"];                        
+                                ?></label>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary" name="updtpr" style="background-color: #666; border-color:#222">Update profile ?</button>
+                    </div>
+                    <div class="text-center">
+                        <br>
+                        <button type="submit" class="btn btn-primary" name="cngpass" style="background-color: #666; border-color:#222">Change Password ?</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/b2.jpg" alt="Second slide">
+        </div>
+    <!-- Bootstrap Carousel -->
+    <div class="container mt-4" id="slider" style="width: 100vw;margin:0;padding:0;">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 100vw;margin:0 0;padding:0 0;">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="b3.png" alt="Slide 1" class="d-block w-100">
+                    <div class="carousel-caption">
+                       
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="b2.jpg" alt="Slide 2" class="d-block w-100">
+                    <div class="carousel-caption">
+                        
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="b1.png" alt="Slide 3" class="d-block w-100">
+                    <div class="carousel-caption">
+                        
+                    </div>
+                </div>
+            </div>
+
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/b3.png" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-    </div>
+
+
+    <script>
+        var profile=document.getElementById("prof");
+        var nav = document.getElementById("nav");
+        var slider = document.getElementById("slider");
+        var close = document.getElementById("close");
+    
+        document.getElementById("name").addEventListener("click",function(){
+            event.preventDefault();             // default rite a tag link per redirect thay.... to aa default vastu je automatic thay tene prevent kersse che
+            profile.style.display="block";
+            nav.style.filter="blur(10px)";
+            slider.style.filter="blur(10px)";
+        })
+
+        document.getElementById("close").addEventListener("click",function(){
+            event.preventDefault(); 
+            profile.style.display="none";
+            nav.style.filter="blur(0px)";
+            slider.style.filter="blur(0px)";
+        })
+
+
+    </script>
 </body>
 </html>
-
-
-
-  
