d9fce2e02b3b61665f4737dfcbe8ff667fc754cd
Umang <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-09-07 12:30:07 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,8 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Page</title>
+    <title>register-1 </title>
     <link rel="stylesheet" href="signup.css">
+    <link rel="shortcut icon" type="x-icon" href="img/browser.jpg" style="background-color: white;" >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 
@@ -13,6 +17,10 @@
     <div id="page1">
         <div id="divlg">
             <div class="registration-container">
+            <a class="navbar-logo" href="#">
+                    <img src="img\log2.png" alt="" style="width: 200px;height: auto;">
+                </a>
+                <hr>
                 <h2>Register-1</h2>
                 <form action="#" method="post" onsubmit="return validateForm();">
                     <p><a href="login.php" id="aha">Already have an account?</a></p>
@@ -72,7 +80,7 @@ function sendMail($email, $fullName,$userName)
         $mail->Subject = 'Password Link For BeatBoxify';
         $mail->Body    = "Response of your forgot Password  <br> <b> Click link below </b>
                             <br>
-                            <a href='http://localhost/proj/signup2.php?email=$email&fname=$fullName&uname=$userName'> Reset Password</a>";
+                            <a href='http://localhost/proj/enter_system/signup2.php?email=$email&fname=$fullName&uname=$userName'>Set Password</a>";
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
@@ -84,29 +92,28 @@ function sendMail($email, $fullName,$userName)
 
 if (isset($_POST['register'])) {
     $fullName = $_POST['full_name'];
-    $userName = $_POST['user_name'];
-    $email = $_POST['email'];
+            $userName = $_POST['user_name'];
+            $email = $_POST['email'];
 
-    if (!empty($fullName) && strpos($fullName, ' ') !== false && !preg_match('/[0-9]/', $fullName)) {
-        if (strlen($userName) >= 5 && preg_match("/^[a-z0-9]+$/", $userName)) {
-            if (filter_var($email, FILTER_VALIDATE_EMAIL) && checkdnsrr(substr(strrchr($email, '@'), 1), 'MX')) {
-
-
-                $result = mysqli_query($connection, "SELECT `fname`, `uname`, `email`, `pass`, `token`, `token_exp` FROM `demo_reg` WHERE `email`='$email'");
-                if (mysqli_num_rows($result)>0) {
-                    echo "<script>
-                    swal(' ERROR', 'Already Have an account with provided Email ', 'Error');
-                    </script>";
-                }
-                else{
-                echo "okka so new email address";    
-                
-                $result = mysqli_query($connection, "SELECT `fname`, `uname`, `email`, `pass`, `token`, `token_exp` FROM `demo_reg` WHERE `uname`='$userName'");
-                if (mysqli_num_rows($result)>0) {
-                    echo "<script>
-                    swal(' ERROR', 'User-name already exist', 'Error');
-                    </script>";
-                } else {
+            if (preg_match('/^[A-Za-z]+\s+[A-Za-z]+$/', trim($fullName)) && !preg_match('/[0-9]/', $fullName)) {
+                if (strlen($userName) >= 5 && preg_match("/^[a-z0-9]+$/", $userName)) {
+                    $localPart = substr($email, 0, strpos($email, '@'));
+                    if (strlen($localPart) > 4  && filter_var($email, FILTER_VALIDATE_EMAIL) && checkdnsrr(substr(strrchr($email, '@'), 1), 'MX')  ) {
+                        $result = mysqli_query($connection, "SELECT `fname`, `uname`, `email`, `pass`, `token`, `token_exp` FROM `demo_reg` WHERE `email`='$email'");
+                        if (mysqli_num_rows($result)>0) {
+                            echo "<script>
+                            swal(' ERROR', 'Already Have an account with provided Email ', 'Error');
+                            </script>";
+                        }
+                        else{
+                        echo "okka so new email address";    
+                        
+                        $result = mysqli_query($connection, "SELECT `fname`, `uname`, `email`, `pass`, `token`, `token_exp` FROM `demo_reg` WHERE `uname`='$userName'");
+                        if (mysqli_num_rows($result)>0) {
+                            echo "<script>
+                            swal(' ERROR', 'User-name already exist', 'Error');
+                            </script>";
+                        } else {
                    
                     if( sendMail($_POST['email'],$_POST['full_name'],$_POST['user_name']) ) {
                         
@@ -120,25 +127,26 @@ if (isset($_POST['register'])) {
                     }
                 }
             }
-            } else {
-                echo "<script>
-                swal('Error!', 'Username should be at least 5 characters long and contain only lowercase letters and numbers.', 'error');
-                </script>";
-            }
+                } else {
+                    echo "<script>
+                    swal('Invalid Email Address !!', '', 'error');
+                    </script>";
+                }
         } else {
             echo "<script>
-            swal('Error!', 'Username should be at least 5 characters long and contain only lowercase letters and numbers.', 'error');
+            swal('Incalid Username!', 'Username should be at least 5 characters long and contain only `a-z` and `0-9`.', 'error');
             </script>";
         }
     } else {
         echo "<script>
-        swal('Error!', 'Full Name should contain firstname and lastname       Eg:: Cristiano Ronaldo', 'error');
+        swal('Incalid Fullname!', 'Full Name should contain firstname and lastname       Eg:: Cristiano Ronaldo', 'error');
         </script>";
     }
 
     // Hash the password using password_hash
     // $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 }
+
 
 ?>
 
